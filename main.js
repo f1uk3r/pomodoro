db113100623cf2ee97f12141c032614d1ee2ca22
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-23 15:19:46 +0530

    added sheetdb api

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let timerMinute = 20
 let timerSecond = 0
+let interval = 20
 let startButton = document.querySelector('.start')
 let timerEnd = new Date()
 let timerAlert = document.querySelector('#alert')
+let pomodoroContent = document.querySelector('#pomodoro-content')
+let postButton = document.querySelector('.post')
+
 function main () {
   startButton.addEventListener('click', () => {
     timer()
+  })
+  postButton.addEventListener('click', () => {
+    postPomodoroContent()
   })
 }
 
@@ -14,7 +21,7 @@ main()
 function initialiseTimer () {
   var now = new Date().getTime()
   //https://stackoverflow.com/questions/1197928/how-to-add-30-minutes-to-a-javascript-date-object
-  timerEnd = new Date(now + 20*60000)
+  timerEnd = new Date(now + interval*60000)
   let totalMinutes = timerEnd - now
   return totalMinutes
 }
@@ -46,4 +53,20 @@ function updateTimer () {
 
 function ringAlert () {
   timerAlert.play()
+}
+
+function postPomodoroContent () {
+  let d = new Date()
+  let date = `${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}`
+  let time = `${d.getHours()}:${d.getMinutes()+1}`
+  axios.post('https://sheetdb.io/api/v1/jq2vlu4cydfwc', {
+    "data": [{
+      "Date": date,
+      "Time": time,
+      "Interval": interval,
+      "Comments": pomodoroContent.value
+    }]
+  })
+  .then((response) => alert(response.data))
+  .catch((error) => console.log(error))
 }
